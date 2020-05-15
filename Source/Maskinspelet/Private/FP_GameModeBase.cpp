3d8cdb1737eda8fbe795fe9e7027544bfcc332b7
gussim-8 <gussim-8@student.ltu.se>
2020-05-15 15:58:37 +0200

    basic main menu structure added

--- Source/Maskinspelet/Private/FP_GameModeBase.cpp
+++ Source/Maskinspelet/Private/FP_GameModeBase.cpp
@@ -10,9 +10,6 @@ AFP_GameModeBase::AFP_GameModeBase()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Blueprints/Source/BP_Character"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Blueprints/Source/Character/BP_Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
-
-	// use our custom HUD class
-	HUDClass = AFP_HUD::StaticClass();
 }
